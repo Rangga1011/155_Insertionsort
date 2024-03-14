cfb7a0056df8a4af0fe9ac1f606a218990c6cf3d
Rangga1011 <ranggaramadhana1011@gmail.com>
2024-03-14 10:37:11 +0700

    Menambahkan Perulangan For

--- Pertemuan4_KelasD_155/Pertemuan4_KelasD_155.cpp
+++ Pertemuan4_KelasD_155/Pertemuan4_KelasD_155.cpp
@@ -22,6 +22,12 @@ void input() {                // Precedure input
     cout << "=====================" << endl; // Membuat tampilan susunan data element array
     cout << "Masukan Element Array" << endl;
     cout << "=====================" << endl;
+
+    for (int i = 0; i < n; i++)              // Menggunakan perulangan for untuk menyimpan data pada array
+    {
+        cout << "Data ke-" << (i + 1) << ":";    // Memasukan atau menginputkan nilai data n
+        cin >> arr[i];                          // Menyimpan nilai data n kedalam array arr
+    }
      
 }
 
